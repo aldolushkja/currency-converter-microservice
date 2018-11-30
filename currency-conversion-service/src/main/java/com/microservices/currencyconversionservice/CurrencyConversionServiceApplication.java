c249847f3178eaecccb7eca929900a5fe97315b4
firetaurus <aldo.lushkja@gmail.com>
2018-11-30 21:34:06 +0100

    running client side load balancing with Ribbon

--- currency-conversion-service/src/main/java/com/microservices/currencyconversionservice/CurrencyConversionServiceApplication.java
+++ currency-conversion-service/src/main/java/com/microservices/currencyconversionservice/CurrencyConversionServiceApplication.java
@@ -2,10 +2,12 @@ package com.microservices.currencyconversionservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients("com.microservices.currencyconversionservice")
+@RibbonClient(name = "currency-exchange-service", configuration = LocalRibbonClientConfiguration.class)
 public class CurrencyConversionServiceApplication {
 
 	public static void main(String[] args) {
